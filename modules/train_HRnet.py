3c09ab77d6a2ee1adb78d3d6708a60cc507c3867
gulierus <ruslan.guliev@seznam.cz>
2025-11-25 09:42:37 +0100

    Add gradient clip in def train epoch

--- modules/train_HRnet.py
+++ modules/train_HRnet.py
@@ -50,6 +50,7 @@ def train_epoch(model, dataloader, optimizer, device, loss_fn=combined_loss):
         loss = loss_fn(outputs, masks)
         
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         
         total_loss += loss.item()
